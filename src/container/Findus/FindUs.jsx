04011a80b49c2e35aae67f4150b24fcccceef747
Thejas Hari <107091644+thejas-dev@users.noreply.github.com>
2023-10-23 21:21:05 +0530

    Update FindUs.jsx

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -10,7 +10,7 @@ const FindUs = () => (
   			Find Us
   		</h1>
   		<div className="app__wrapper-content">
-  			<p className="p__opensans" >Barathiyar theru,Dubai kurukku sendhu,Dubai Main Road,Dubai.</p>
+  			<p className="p__opensans" >123 Main Street, Dubai, UAE.</p>
   			<p className="p__cormorant" style={{color:'#DCCA87',margin:'2rem 0'}} >Opening Hours</p>
   			<p className="p__opensans" >Mon - Fri: 10:00 am - 02:00 am</p>
   			<p className="p__opensans" >Sat - Sun: 10:00 am - 3:00 am</p>
